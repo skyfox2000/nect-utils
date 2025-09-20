4918d5622dc009ff90c33cf227fdfe521d738169
hlong <hlong@rainect.com>
2025-09-20 21:41:47 +0800

    fix(crypt): 修正crypto模块变量命名错误
    - 将变量名从crytoModule修正为cryptoModule
    - 修复JSModules中crypto模块赋值的变量名对应错误
    - 保证crypto模块正确初始化和注册
    - 修正相关代码的拼写错误，提高代码可读性和稳定性

--- jsmodule/crypt.go
+++ jsmodule/crypt.go
@@ -4,12 +4,12 @@ import (
 	"github.com/skyfox2000/nect-utils/encrypt"
 )
 
-var crytoModule = map[string]interface{}{
+var cryptoModule = map[string]interface{}{
 	"md5": func(data string) string {
 		return encrypt.MD5(data)
 	},
 }
 
 func init() {
-	JSModules["crypto"] = crytoModule
+	JSModules["crypto"] = cryptoModule
 }
